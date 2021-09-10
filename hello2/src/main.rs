9a37fda2111e5aa693359ee889373fdeb989b5c1
larryf <larry@fastmail>
2021-09-09 22:00:09 -0400

    lifetimes

--- hello2/src/main.rs
+++ hello2/src/main.rs
@@ -1,15 +1,11 @@
 fn main() {
-  let n = 12345678;
+  let a = 2;
+  let b = 5;
 
-  let desc = if is_even(n) {
-    "even"
-  } else {
-    "odd"
-  };
-
-  println!("{}", desc);
+  let res = add_with_lifetimes( &a, &b );
+  println!("{}", res);
 }
 
-fn is_even(n: i32) -> bool {
-  n % 2 == 0
+fn add_with_lifetimes<'a, 'b>( i: &'a i32, j: &'b i32) -> i32 {
+  *i + *j
 }
