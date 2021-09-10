a25158518049844b0753368cfe1d994b5ac35f1b
larryf <larry@fastmail>
2021-09-09 21:23:13 -0400

    conditional assignment

--- hello2/src/main.rs
+++ hello2/src/main.rs
@@ -1,10 +1,15 @@
-use std::rc::Rc;
-use std::sync::{Arc, Mutex};
-
 fn main() {
-  let a = 10;
-  let b = Box::new(20);
-  let c = Rc::new(Box::new(30));
-  let d = Arc::new(Mutex::new(40));
-  println!(" a: {:?}, b: {:?}, c: {:?}, d: {:?}", a,b,c,d);
+  let n = 12345678;
+
+  let desc = if is_even(n) {
+    "even"
+  } else {
+    "odd"
+  };
+
+  println!("{}", desc);
+}
+
+fn is_even(n: i32) -> bool {
+  n % 2 == 0
 }
